9ec33c2c1a33b536440d37de071d498e6c3e42e0
Chimaobi Amadi <maobiamadi@gmail.com>
2020-02-29 11:47:22 -0800

    Adding numeric scaler function

--- R/PrepR.R
+++ R/PrepR.R
@@ -29,3 +29,28 @@ data_type <- function(df) {
 #' onehot(my_data)
 data_type <- function(encodable_df) {
 }
+
+#' scaler
+#' Perform standard scaler on numerical features.
+#'
+#' @param x_train data.frame
+#' Dataframe of train set containing columns to be scaled.
+#' 
+#' @param x_test data.frame
+#' Dataframe of test set containing columns to be scaled.
+#' 
+#' @param colnames vector
+#' Vector of column names for numeric features
+#' 
+#' @return list of data.frame
+#' Stores the x_train and x_test separately as two dataframes in one list.
+#' The first element in the list will contain x_train and the second will contain x_test.
+#'
+#' @examples
+#' x_train <- tibble(colors = c('Blue', 'Red', 'Green'), counts = c(34, 35, 56), usage = c(4, 6, 9))
+#' x_test <- tibble(colors = c('Blue', 'Red', 'Green'), counts = c(20, 35, 18), usage = c(9, 6, 0))
+#' colnames <- c('counts', 'usage')
+#' 
+#' scaler(x_train, x_test, colnames)
+scaler <- function(x_train, x_test, colnames) {
+}
