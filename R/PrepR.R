4b3799fe1ac1d0297e50390128a1fec8cb532579
Matt <matthew.at.connell@gmail.com>
2020-03-02 17:57:38 -0800

    removed ohe from PrepR.R

--- R/PrepR.R
+++ R/PrepR.R
@@ -14,3 +14,83 @@
 data_type <- function(df) {
 }
 
+#' train_valid_test
+#' Split arrays or matrices into random train, validation and test subsets
+#'
+#' @param X data.frame
+#' Sequence of indexable of same length as y
+#' Contains the original features from the data set
+#'
+#' @param y data.frame
+#' Sequence of indexable of same length as X
+#' Contains the original features from the data set
+#'
+#' @param test_size numeric, integer or None, optional (default=None).
+#' If numeric, a value between 0.0 and 1.0 to represent the proportion of the dataset to comprise the size of the test subset
+#' If integer, a value for the absolute number of test samples
+#' If None, the value is set to the complement of the sum of the train_size and test_size
+#' If train_size and test_size are also None, the value is set to 0.25
+#'
+#' @param valid_size numeric, integer or None, (default=None)
+#' If numeric, a value between 0.0 and 1.0 to represent the proportion of the dataset to comprise the size of the test subset
+#' If integer, a value for the absolute number of validation samples
+#' If None, the value will be set to 0.0
+#'
+#' @param train_size numeric, integer or None, (default=None)
+#' If numeric, a value between 0.0 and 1.0 to represent the proportion of the dataset to comprise the size of the test subset
+#' If integer, a value for the absolute number of train samples
+#' If None, the value will be set to 0.0
+#'
+#' @param stratify data.frame or None (default=None)
+#' If not None, splits categorical data in a stratified fashion preserving the same proportion of classes in the train, valid and test sets, using this input as the class labels
+#'
+#' @param random_state integer, optional (default=None)
+#' A value for the seed to be used by the random number generator
+#' If None, the value will be set to 1
+#'
+#' @param shuffle: boolean, optional (default=TRUE)
+#' Indicate whether data is to be shuffled prior to splitting
+#'
+#' @return list, length=(3 * length(X))
+#' Containis train, validation and test splits of the input data
+#'
+#' @examples
+#' X <- tibble(c(0, 10), c(2, 3), c(4, 5), c(6, 7),
+#'             c(8,9), c(10, 11), c(12, 13), c(14, 15)),
+#' Y <- tibble(0, 1, 2, 3, 4, 5, 6, 7)
+train_valid_test <- function(X, y, test_size, valid_size, train_size,
+                             stratify, random_state, shuffle) {
+}
+
+
+#' scaler
+#' Perform standard scaler on numerical features.
+#'
+#' @param x_train data.frame
+#' Dataframe of train set containing columns to be scaled.
+#'
+#' @param x_valid data.frame
+#' Dataframe of validation set containing columns to be scaled.
+#'
+#' @param x_test data.frame
+#' Dataframe of test set containing columns to be scaled.
+#'
+#' @param colnames vector
+#' Vector of column names for numeric features
+#'
+#' @return list of data.frame
+#' Stores the x_train, x_valid and x_test separately as three dataframes in one list.
+#' The first element in the list will contain x_train, second will be x_valid and the third will contain x_test.
+#'
+#' @examples
+#' x_train <- tibble(colors = c('Blue', 'Red', 'Green'), counts = c(34, 35, 56), usage = c(4, 6, 9))
+#'
+#' x_valid <- tibble(colors = c('Blue', 'Red', 'Green'), counts = c(29, 65, 13), usage = c(5, 27, 10))
+#'
+#' x_test <- tibble(colors = c('Blue', 'Red', 'Green'), counts = c(20, 35, 18), usage = c(9, 6, 0))
+#'
+#' colnames <- c('counts', 'usage')
+#'
+#' scaler(x_train, x_test, colnames)
+scaler <- function(x_train, x_valid, x_test, colnames) {
+}
