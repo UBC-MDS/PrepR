25c1ca987f76185a575f6ef2b12021817740671b
Matt <matthew.at.connell@gmail.com>
2020-03-03 13:33:51 -0800

    initial framework, not yet functional

--- R/onehot.R
+++ R/onehot.R
@@ -1,5 +1,5 @@
 #' One-Hot encode a dataframe
-#' 
+#'
 #' One-hot encodes a dataframe whose features are all categorical
 #'
 #' @param encodable_df data.frame
@@ -12,4 +12,30 @@
 #' my_data <- tibble(animals = c('monkey', 'dog', 'cat'))
 #' onehot(my_data)
 onehot <- function(encodable_df) {
+
+
+  # Helper data
+  fruits <- data.frame(species = c("apple", "grape", "pear"))
+  fruits$species <- as.factor(fruits$species)
+
+
+  columns <- c()
+  for (level in levels(fruits$species)) {
+    columns <- c(columns, level)
+  }
+
+  columns2 <- c()
+
+  for (column in columns) {
+    assign(column, rep(0,3))
+    columns2 <- c(columns2, as.name(column))
+  }
+
+  df <- data.frame(eval(columns2[[1]]))
+
+  for (i in seq(2, length(columns2))) {
+    df <- cbind(df, eval(columns2[[i]]))
+  }
+
+  names(df) <- columns
 }
