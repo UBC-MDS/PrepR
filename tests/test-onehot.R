c0a3c75c3a25ee67d01feebf86b6af3528ab7feb
Matt <matthew.at.connell@gmail.com>
2020-03-07 11:37:36 -0800

    added tests to function

--- tests/test-onehot.R
+++ tests/test-onehot.R
@@ -2,24 +2,25 @@ library(testthat)
 library(PrepR)
 
 # Helper data
-fruits <- data.frame(species = c("apple", "grape", "pear"))
+fruits <- data.frame(species = c("apple", "grape", "pear", "banana"))
 fruits$species <- as.factor(fruits$species)
 
 output = onehot(fruits)
 
-test_that("Length of input and output should be equal", {
+test_that("Length of input and output should be equal (same number of rows)", {
   expect_equal(length(fruits[[1]]), length(output[[1]]))
 })
 
-levels(test[[names(test)]])
 test_that("There should not be more than 10 unique values", {
   expect_error(onehot(mtcars$mpg))
 })
 
+test_that("Number of columns in output should be equal to number of unique values in input column",{
+  output <- onehot(fruits)
+  expect_equal(length(fruits[[1]]), length(output))
+})
 
-
-
-onehot(test)
-
-
-test[5,1] ==8
+test_that("Each row should add up to 1", {
+  for (i in 1:nrow(output)) {
+    expect_equal(sum(output[i,]), 1)}
+})
