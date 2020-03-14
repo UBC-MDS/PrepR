9be8a2f7edb984ab26f92e37e3282ed139314c2b
George <george,.thio@gmail.com>
2020-03-13 21:25:04 -0700

    Fixed if statement

--- R/scaler.R
+++ R/scaler.R
@@ -44,9 +44,9 @@ scaler <- function(X_train, x_valid, X_test, colnames) {
     stop("Input data must be data.frame object")
   }
   
-  if (length(X_train) == 0 | length(X_test) == 0 | length(x_valid == 0 | length(colnames) == 0)) {
+  if (length(X_train) == 0 | length(X_test) == 0 | length(x_valid) == 0 | length(colnames) == 0) {
     stop("Input data cannot be empty")
-  }
+    }
 
   X_train_scaled <- data.frame(X_train)
   X_train_scaled[colnames] <- scale(X_train[colnames])
