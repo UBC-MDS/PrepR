e2cfa3ec9ef48ecc02e00380f48983131213379f
George <george,.thio@gmail.com>
2020-03-13 20:40:47 -0700

    Fixed  and  conflict

--- R/scaler.R
+++ R/scaler.R
@@ -4,7 +4,8 @@
 #' @param x_train data.frame
 #' Dataframe of train set containing columns to be scaled.
 #'
-#' @param x_valid data.frame
+#' @param x_valid
+ data.frame
 #' Dataframe of validation set containing columns to be scaled.
 #'
 #' @param x_test data.frame
@@ -16,46 +17,63 @@
 #' @export
 #' 
 #' @return list of data.frame
-#' Stores the x_train, x_valid and x_test separately as three dataframes in one list.
-#' The first element in the list will contain x_train, second will be x_valid and the third will contain x_test.
+#' Stores the x_train, x_valid
+ and x_test separately as three dataframes in one list.
+#' The first element in the list will contain x_train, second will be x_valid
+ and the third will contain x_test.
 #'
 #' @examples
 #' x_train <- data.frame(colors = c('Blue', 'Red', 'Green'), counts = c(34, 35, 56), usage = c(4, 6, 9))
 #'
-#' x_validation <- data.frame(colors = c('Blue', 'Red', 'Green'), counts = c(29, 65, 13), usage = c(5, 27, 10))
+#' x_valid
+ation <- data.frame(colors = c('Blue', 'Red', 'Green'), counts = c(29, 65, 13), usage = c(5, 27, 10))
 #'
 #' x_test <- data.frame(colors = c('Blue', 'Red', 'Green'), counts = c(20, 35, 18), usage = c(9, 6, 0))
 #'
 #' colnames <- c('counts', 'usage')
 #'
-#' X_train = scaler(x_train, x_validation, x_test, colnames)$X_train
-#' X_test = scaler(x_train, x_validation, x_test, colnames)$X_test
-#' X_validation = scaler(x_train, x_validation, x_test, colnames)$X_validation
+#' X_train = scaler(x_train, x_valid
+, x_test, colnames)$X_train
+#' X_test = scaler(x_train, x_valid
+ation, x_test, colnames)$X_test
+#' x_valid
+ation = scaler(x_train, x_valid
+ation, x_test, colnames)$x_valid
+ation
 
 
-scaler <- function(X_train, X_validation, X_test, colnames) {
+scaler <- function(X_train, x_valid
+ation, X_test, colnames) {
   library(testthat)
 
-  if (!inherits(X_train, "data.frame") | !inherits(X_test, "data.frame") | !inherits(X_validation, "data.frame")) {
+  if (!inherits(X_train, "data.frame") | !inherits(X_test, "data.frame") | !inherits(x_valid
+  ation, "data.frame")) {
     stop("Input data must be data.frame object")
   }
   
-  if (length(X_train) == 0 | length(X_test) == 0 | length(X_validation) == 0 | length(colnames) == 0) {
+  if (length(X_train) == 0 | length(X_test) == 0 | length(x_valid
+  ation) == 0 | length(colnames) == 0) {
     stop("Input data cannot be empty")
   }
 
   X_train_scaled <- data.frame(X_train)
   X_train_scaled[colnames] <- scale(X_train[colnames])
 
-  X_validation_scaled <- data.frame(X_validation)
-  X_validation_scaled[colnames] <- scale(X_validation[colnames])
+  x_valid
+  ation_scaled <- data.frame(x_valid
+  ation)
+  x_valid
+  ation_scaled[colnames] <- scale(x_valid
+  ation[colnames])
 
   X_test_scaled <- data.frame(X_test)
   X_test_scaled[colnames] <- scale(X_test[colnames])
 
 
-  scaled_data <- list(X_train_scaled, X_validation_scaled, X_test_scaled)
-  names(scaled_data) <- c('X_train', 'X_validation', 'X_test')
+  scaled_data <- list(X_train_scaled, x_valid
+  ation_scaled, X_test_scaled)
+  names(scaled_data) <- c('X_train', 'x_valid
+  ation', 'X_test')
 
   return(scaled_data)
 }
