f71eecaa376444a52387b2b247001dee7f310345
Matt <matthew.at.connell@gmail.com>
2020-03-26 12:22:21 -0700

    addressed feedback for function onehot.
    -Added functionality: can now take input with more than one column
    -Added tests
    -Added functionality: new argument: 'drop_first' allows user to drop first column of dataframe
    -Better column names: now returns columns of the format 'columnname_valuename'. Example: 'animal_monkey' instead of just 'monkey'. This works better for input with multiple columns

--- R/onehot.R
+++ R/onehot.R
@@ -9,11 +9,11 @@
 #' Returns the same dataframe with useful column names and one-hot encoded features
 #'
 #' @export
-#' 
+#'
 #' @examples
 #' my_data <- data.frame(animals = c('monkey', 'dog', 'cat'))
 #' onehot(my_data)
-onehot <- function(encodable_df) {
+onehot <- function(encodable_df, drop_first = FALSE) {
 
   # Input type check:
   if (is.data.frame(encodable_df) ==FALSE) {
@@ -25,30 +25,67 @@ onehot <- function(encodable_df) {
   stop("The column should have fewer than 10 unique values")
   }
 
+  # For inputs with multiple columns:
+  if (ncol(encodable_df) > 1) {
+
+    # Input type check:
+    if (is.data.frame(encodable_df) ==FALSE) {
+      stop("Input should be a dataframe!")
+    }
+
+    encoded_df <- onehot(encodable_df[1], drop_first = drop_first)
+
+    for (i in 2:length(encodable_df)) {
+
+      oh_column = onehot(encodable_df[i], drop_first = drop_first)
+      encoded_df<- cbind(encoded_df, oh_column)
+
+    }
+
+    return(encoded_df)
+
+  }
+
   # For one column df:
   header <- names(encodable_df)
 
+
+  # Rename the columns
   columns <- c()
   for (level in levels(encodable_df[[header]])) {
     columns <- c(columns, level)
   }
 
+  # Make a column of zeroes for each column
   columns2 <- c()
-
   for (column in columns) {
     assign(column, rep(0,nrow(encodable_df)))
+
+    # Bind the columns together
     columns2 <- c(columns2, as.name(column))
   }
 
   df <- data.frame(eval(columns2[[1]]))
 
-  for (i in seq(2, length(columns2))) {
-    df <- cbind(df, eval(columns2[[i]]))
+  if (length(columns2) > 1) {
+
+    if (is.list(columns2) == FALSE) {
+      stop("There's a problem with your column names.")
+      }
+
+    for (i in 2:length(columns2)) {
+      df <- cbind(df, eval(columns2[[i]]))
+    }
+
   }
 
   names(df) <- columns
 
+  # Count number of instances for each value
   for (name in names(df)) {
+
+    #name <- str_split(name, "_")[[1]][2]
+
     for (i in seq(1,nrow(encodable_df))) {
 
       if (encodable_df[i,1] == name) {
@@ -60,5 +97,24 @@ onehot <- function(encodable_df) {
     }
   }
 
+  # Rename the columns
+  columns <- c()
+  for (level in levels(encodable_df[[header]])) {
+    level <- paste0(header, "_", level)
+    columns <- c(columns, level)
+  }
+
+  names(df) <- columns
+
+  if (drop_first==TRUE) {
+
+    if (length(df) > 1) {
+
+        df <- drop(df[-1])
+      }
+    }
+
   df
+
 }
+
